f5cea1bdf6c8e62f37b63475764e4bd3aa2ebba3
jpluengo2 <jpluengo2@gmail.com>
2025-12-04 10:40:34 +0100

    Initial Commit

---
+++ Movies-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies-IOS
+//
+//  Created by Mananas on 4/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
